9109ce241a7c0902d28e2e7d3e9b6fb0ae1906e9
Hiemswolf <claytonkruse08@gmail.com>
2023-09-07 15:55:26 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "./style.scss";
+</script>
+
+<main id="main" class="container">
+    <slot />
+</main>
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,109 @@
+<h1>Resume</h1>
+<section class="knowledgebase">
+    <h2>Knowledgebase</h2>
+    <section>
+        <h3>Languages</h3>
+        <ul>
+            <li>C</li>
+            <li>
+                Javascript
+                <ul>
+                    <li>Typescript</li>
+                    <li>
+                        JSON
+                        <ul>
+                            <li>MongoDB</li>
+                        </ul>
+                    </li>
+                    <li>
+                        NodeJS
+                        <ul>
+                            <li>SvelteKit</li>
+                            <li>ExpressJS</li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li>HTML</li>
+            <li>
+                CSS
+                <ul>
+                    <li>SASS</li>
+                    <li>LESS</li>
+                </ul>
+            </li>
+            <li>Java</li>
+            <li>
+                SQL
+                <ul>
+                    <li>MySQL</li>
+                    <li>SQLite</li>
+                    <li>PosgreSQL</li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+    <section>
+        <h3>Operating Systems</h3>
+        <ul>
+            <li>
+                GNU/Linux
+                <ul>
+                    <li>Debian</li>
+                    <li>
+                        <span
+                            data-tooltip="The first Linux distribution I really learned."
+                            data-placement="right">Arch Linux</span
+                        >
+                        <ul>
+                            <li>
+                                <span
+                                    data-tooltip="I have been daily driving EndeavourOS for a year now."
+                                    data-placement="right">EndeavourOS</span
+                                >
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li>Windows</li>
+        </ul>
+    </section>
+    <section>
+        <h3>Editors</h3>
+        <ul>
+            <li>
+                Vi
+                <ul>
+                    <li>
+                        Vim
+                        <ul><li>Neovim</li></ul>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                VSCode
+                <ul><li>VSCodium</li></ul>
+            </li>
+        </ul>
+    </section>
+    <section>
+        <h3>Misc.</h3>
+        <ul>
+            <li>
+                <a
+                    href="https://caddyserver.com"
+                    data-tooltip="A http Server"
+                    data-placement="right">Caddy</a
+                >
+            </li>
+        </ul>
+    </section>
+</section>
+
+<style>
+    /* .knowledgebase {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+    } */
+</style>
